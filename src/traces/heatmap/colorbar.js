02fc9aceced50497b8000b0ed001aaf0456e22ce
Robert Monfera <monfera.robert@gmail.com>
2017-01-12 14:11:55 +0100

    removed more TODOs

--- src/traces/heatmap/colorbar.js
+++ src/traces/heatmap/colorbar.js
@@ -16,7 +16,6 @@ var Plots = require('../../plots/plots');
 var Colorscale = require('../../components/colorscale');
 var drawColorbar = require('../../components/colorbar/draw');
 
-// TODO unify parcoords, scatter and heatmap colorbar
 module.exports = function colorbar(gd, cd) {
     var trace = cd[0].trace,
         cbId = 'cb' + trace.uid,
